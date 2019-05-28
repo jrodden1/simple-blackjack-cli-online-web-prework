a18ce5727c4b779a8666d01029964c4c551b94d2
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-28 18:50:03 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -64,13 +64,11 @@ end
 
 def runner
   require_relative "blackjack.rb"
-  require "pry"
   welcome
   current_total = initial_round
   until current_total > 21 do
     current_total = hit?(current_total)
     display_card_total(current_total)
-    binding.pry
   end
   end_game(current_total)
 end
