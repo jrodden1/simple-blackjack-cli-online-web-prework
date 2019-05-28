1a901103b8605c36e138881c836a2fac779386f1
communal-tutorial-3267 <jrodden1@gmail.com>
2019-05-28 18:37:05 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,7 +1,7 @@
 require_relative "blackjack.rb"
 welcome
 currentCount = initial_round
-while currentCount <= 21 do
+while currentCount < 21 do
   currentCount = hit?(currentCount)
   display_card_total(currentCount)
 end
